2fefae2d0437f0e7d4361a58033635a1de4e491b
Nikita Andrejevs <nikita@niki-mac.local>
2019-08-22 22:20:51 +0300

    load filesl ocally

--- src/Page.jsx
+++ src/Page.jsx
@@ -1,8 +1,15 @@
-import React, { useState } from 'react';
-import generator from './utils/generator';
-import LinePlot from './Plots';
+// import { useState } from 'react';
+import { csv } from 'd3-fetch';
+// import generator from './utils/generator';
+// import LinePlot from './Plots';
 
 export default () => {
-  const [data] = useState(() => generator(100));
-  return <LinePlot data={data} size={[1000, 500]} />;
+  // const [data] = useState(() => );
+  fetch('../data/eurusd_hour.csv').then(d => console.log('d', d));
+  // const data = csv('../data/eurusd_hour.csv').then(d => console.log('d', d));
+  // console.log('>>> data', data);
+  // const [data] = useState(() => generator(100));
+
+  return null;
+  // return <LinePlot data={data} size={[1000, 500]} />;
 };
