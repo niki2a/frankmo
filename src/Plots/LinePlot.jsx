b5ed838797a6f5990077f5051383588c343dbc6d
Nikita Andrejevs <nikita@niki-mac.local>
2019-08-26 15:12:03 +0300

    use d3 only to calculate the line path

--- src/Plots/LinePlot.jsx
+++ src/Plots/LinePlot.jsx
@@ -1,12 +1,12 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 
 import { scaleLinear, scaleTime } from 'd3-scale';
 import { extent } from 'd3-array';
 import { line } from 'd3-shape';
-import { select } from 'd3-selection';
+// import { select } from 'd3-selection';
 
-function createBarChart(node, size, data) {
+function createLinePlot(node, size, data) {
   if (!node || !node.current || !data) {
     return undefined;
   }
@@ -21,35 +21,44 @@ function createBarChart(node, size, data) {
     .domain(extent(data, d => +d.price))
     .range([height, 0]);
 
-  const svg = select(node.current)
-    .attr('width', width)
-    .attr('height', height)
-    .append('g')
-    .attr('style', 'fill: blue;');
+  // const svg = select(node.current)
+  //   .attr('width', width)
+  //   .attr('height', height)
+  //   .append('g');
+
+  // .selectAll('g');
+  // .enter()
+  // .data(data);
 
   const visualLine = line()
     .x(d => xScale(d.date))
     .y(d => yScale(d.price));
 
-  svg
-    .append('path')
-    .attr('style', 'stroke: steelblue;stroke-width: 2;fill: none;')
-    .attr('d', visualLine(data));
+  // svg
+  //   .append('path')
+  //   .attr('style', 'stroke: steelblue;stroke-width: 2;fill: none;')
+  //   .attr('d', visualLine(data));
 
-  return undefined;
+  // svg.exit().remove();
+  return visualLine(data);
 }
 
 const LinePlot = ({ size, data }) => {
   const [width, height] = size;
   const node = useRef(null);
+  const [linePath, setLinePath] = useState(null);
+
   useEffect(() => {
-    if (data.length === 0) {
-      return;
-    }
-    createBarChart(node, size, data);
+    setLinePath(createLinePlot(node, size, data));
   }, [node, size, data]);
 
-  return <svg ref={node} width={width} height={height} />;
+  return (
+    <svg ref={node} width={width} height={height}>
+      <g>
+        <path d={linePath} />
+      </g>
+    </svg>
+  );
 };
 
 LinePlot.propTypes = {
